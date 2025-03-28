fb42d93906b196bb034cdcdf8badf229a91b078f
damevi <maciej.urlo@gmail.com>
2025-03-28 09:08:33 +0100

    feat: add feed follows

---
+++ sql/queries/feed_follows.sql
@@ -0,0 +1,25 @@
+-- name: CreateFeedFollow :one
+WITH inserted_feed_follow AS (
+    INSERT INTO feed_follows(id, user_id, feed_id, created_at, updated_at)
+    VALUES ($1, $2, $3, $4, $5)
+    RETURNING *
+)
+SELECT
+    iff.*,
+    f.name AS feed_name,
+    u.name AS user_name
+FROM inserted_feed_follow iff
+JOIN feeds f ON f.id =  iff.feed_id
+JOIN users u ON u.id = iff.user_id;
+--
+
+-- name: GetFeedFollowsForUser :many
+SELECT
+    ff.*,
+    f.name AS feed_name,
+    u.name AS user_name
+FROM feed_follows ff
+JOIN feeds f ON f.id = ff.feed_id
+JOIN users u ON u.id = ff.user_id
+WHERE ff.user_id = $1;
+--
